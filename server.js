d3170e7fd38e3ccd47f2a8f0ab6f2eb1df64cf27
Gauri Khandke <khandkegauri@gmail.com>
2020-11-24 20:24:01 -0800

    Updated express server, created mongoose connection and added routes

--- server.js
+++ server.js
@@ -1,17 +1,33 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const path = require("path");
+const routes = require("./routes");
+
+// dotenv npm package stores configuration in the environment variables
+require('dotenv').config();
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebookssearch",{
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+});
+
 // Define API routes here
+app.use(routes);
 
 // Send every other request to the React app
 // Define any API routes before this runs
