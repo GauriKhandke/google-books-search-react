ce1d7a4e39426b6151bb0bbf39cdeaec3cf3c7b7
Gauri Khandke <khandkegauri@gmail.com>
2020-11-25 18:30:49 -0800

    converted promises to async await functions

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,40 +1,57 @@
-const db = require("../models");
+const db = require('../models');
 
 module.exports = {
+	// Find all saved books
+	findAll: async (req, res) => {
+		try {
+			const books = await db.Book.find(req.query);
+			return res.json(books);
+		} catch (error) {
+			return res.status(422).json(error);
+		}
+	},
 
-  // Find all saved books
-  findAll: function(req, res) {
-    db.Book.find(req.query)
-      .then(dbBook => res.json(dbBook))
-      .catch(err => res.status(422).json(err));
-  },
+	// Find book by ID
+	findById: async (req, res) => {
+		try {
+			const books = await db.Book.findById(req.params.id);
+			return res.json(books);
+		} catch (error) {
+			return res.status(422).json(error);
+		}
+	},
 
-  // Find book by ID
-  findById: function(req, res) {
-    db.Book.findById(req.params.id)
-      .then(dbBook => res.json(dbBook))
-      .catch(err => res.status(422).json(err));
-  },
+	// Create/save book in database
+	create: async (req, res) => {
+		try {
+			const book = await db.Book.create(req.body);
+			return res.json(book);
+		} catch (error) {
+			return res.status(422).json(error);
+		}
+	},
 
-  // Create/save book in database
-  create: function(req, res) {
-    db.Book.create(req.body)
-      .then(dbBook => res.json(dbBook))
-      .catch(err => res.status(422).json(err));
-  },
+	// Update one book by ID
+	update: async (req, res) => {
+		try {
+			const book = await db.Book.findOneAndUpdate(
+				{ id: req.params.id },
+				req.body
+			);
+			return res.json(book);
+		} catch (error) {
+			return res.status(422).json(error);
+		}
+	},
 
-  // Update one book by ID
-  update: function(req, res) {
-    db.Book.findOneAndUpdate({ id: req.params.id }, req.body)
-      .then(dbBook => res.json(dbBook))
-      .catch(err => res.status(422).json(err));
-  },
-
-  // Delete book from database
-  remove: function(req, res) {
-    db.Book.findById(req.params.id)
-      .then(dbBook => dbBook.remove())
-      .then(dbBook => res.json(dbBook))
-      .catch(err => res.status(422).json(err));
-  }
+	// Delete book from database
+	remove: async (req, res) => {
+		try {
+			const book = await db.Book.findById(req.params.id);
+			const deletedBook = await book.remove();
+			return res.json(deletedBook);
+		} catch (error) {
+			return res.status(422).json(error);
+		}
+	},
 };
--- controllers/googleController.js
+++ controllers/googleController.js
@@ -1,46 +1,41 @@
-const axios = require("axios");
-const db = require("../models");
+const axios = require('axios');
+const db = require('../models');
 
-// findAll searches the Google Books API and returns only the entries that are not saved
+// Defining methods for the googleController
 module.exports = {
+  
+  // Searches the Google Books API and returns only unsaved books
+	findAll: async (req, res) => {
+		try {
+			const { query: params } = req;
 
-  findAll: function(req, res) {
+			const results = await axios.get(
+				'https://www.googleapis.com/books/v1/volumes',
+				{ params }
+			);
 
-    // Fetch query parameter
-    const { query: params } = req;
+			const apiBooks = await results.data.items.filter(
+				(result) =>
+					result.volumeInfo.title &&
+					result.volumeInfo.infoLink &&
+					result.volumeInfo.authors &&
+					result.volumeInfo.description &&
+					result.volumeInfo.imageLinks &&
+					result.volumeInfo.imageLinks.thumbnail
+			);
 
-    // Axios GET request to Google Books API
-    axios
-      .get("https://www.googleapis.com/books/v1/volumes", {
-        params
-      })
-      .then(results =>
-        // Fetch required info from response
-        results.data.items.filter(
-          result =>
-            result.volumeInfo.title &&
-            result.volumeInfo.infoLink &&
-            result.volumeInfo.authors &&
-            result.volumeInfo.description &&
-            result.volumeInfo.imageLinks &&
-            result.volumeInfo.imageLinks.thumbnail
-        )
-      )
-      // Books returned from api call after filtering required info
-      .then(apiBooks =>
+			const dbBooks = await db.Book.find();
 
-        // Find all books saved in database and send as dbBooks
-        db.Book.find().then(dbBooks =>
+			const books = await apiBooks.filter((apiBook) =>
+				dbBooks.every(
+					(dbBook) => dbBook.googleId.toString() !== apiBook.id
+				)
+			);
 
-          apiBooks.filter(apiBook =>
-            
-            // Filter and send books that are not saved in database
-            dbBooks.every(dbBook => dbBook.googleId.toString() !== apiBook.id)
-          )
-        )
-      )
-      // Send response back to client
-      .then(books => res.json(books))
-      .catch(err => res.status(422).json(err));
-  }
+      return res.json(books);
+      
+		} catch (error) {
+			return res.status(422).json(error);
+		}
+	},
 };
