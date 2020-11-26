8a393dee414c3a59dbd31e2e36784127babdd79c
Gauri Khandke <khandkegauri@gmail.com>
2020-11-25 17:11:16 -0800

    Modified database name

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ if (process.env.NODE_ENV === "production") {
 }
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebookssearch",{
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks",{
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
