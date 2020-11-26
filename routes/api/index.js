f7a9f7b5afb806bfff62e462e019c66cbf87d837
Gauri Khandke <khandkegauri@gmail.com>
2020-11-25 18:32:49 -0800

    Removed build file from api index.js

--- routes/api/index.js
+++ routes/api/index.js
@@ -9,9 +9,4 @@ router.use("/books", bookRoutes);
 // Google Routes
 router.use("/google", googleRoutes);
 
-// For anything else, render the html page
-router.use(function(req, res) {
-  res.sendFile(path.join(__dirname, "../../client/build/index.html"));
-});
-
 module.exports = router;
